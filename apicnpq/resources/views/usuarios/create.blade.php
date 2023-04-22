9ea218ed80f3fb4d894432b73d1222b26c3160d3
Júlia Souza <juliadsouzaborges@gmail.com>
2023-04-22 10:52:23 -0300

    layout de formulário

--- apicnpq/resources/views/usuarios/create.blade.php
+++ apicnpq/resources/views/usuarios/create.blade.php
@@ -1,78 +1,112 @@
 <x-layout title="Novo Beneficiário">
 <div class="container" >
+    <h1>Cadastro de novo beneficiário</h1>
+    <p>Insira os dados abaixos para realizar o cadastro!</p>
     <form action="#" method="post">
-        <div class="input-group mb-3" style="display: block;">
-            <div class="mb-3">
+        <div class="form-group">
+            <div class="form-group">
                 <label for="beneficiario">Nome do beneficário: </label>
-                <input type="text" id="beneficiario" name="beneficiario">
+                <input class="form-control" class="form-control" type="text" id="beneficiario" name="beneficiario">
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="processo">N° do processo: </label>
+                            <input class="form-control" type="text" id="processo" name="processo">
+                        </div> 
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="valor_pago">Valor da bolsa: </label>
+                            <input class="form-control" type="text" id="valor_pago" name="valor_pago">
+                        </div>
+                    </div>
+                </div>
             </div>
             
-            <div class="mb-3">
-                <label for="processo">N° do processo: </label>
-                <input type="text" id="processo" name="processo">
-            </div>
-            
-            <div class="mb-3">
-                <label for="instituicao">Nome da instituição: </label>
-                <input type="text" id="instituicao" name="instituicao">
+            <div class="form-group">
+                <div class="row">
+                    <div class="col">
+                        <label for="instituicao">Nome da instituição: </label>
+                        <input class="form-control" type="text" id="instituicao" name="instituicao">
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="sigla_instituicao">Sigla da instituição: </label>
+                            <input class="form-control" type="text" id="sigla_instituicao" name="sigla_instituicao">
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div class="mb-3">
-                <label for="sigla_instituicao">Sigla da instituição: </label>
-                <input type="text" id="sigla_instituicao" name="sigla_instituicao">
-            </div>
+           
 
-            <div class="mb-3">
+            <div class="form-group">
                 <label for="programaCNPq">Programa CNPq: </label>
-                <input type="text" id="programaCNPq" name="programaCNPq">
+                <input class="form-control" type="text" id="programaCNPq" name="programaCNPq">
             </div>
 
-            <div class="mb-3">
-                <label for="data_inicio">Data Início: </label>
-                <input type="date" id="data_inicio" name="data_inicio">
+            <div class="form-group">
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="data_inicio">Data Início: </label>
+                            <input class="form-control" type="date" id="data_inicio" name="data_inicio">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="data_termino">Data Término: </label>
+                            <input class="form-control" type="date" id="data_termino" name="data_termino">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="ano_referencia">Ano de referência: </label>
+                            <input class="form-control" type="number" id="ano_referencia" name="ano_referencia" minlength = 4 maxlength = 4>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div class="mb-3">
-                <label for="data_termino">Data Término: </label>
-                <input type="date" id="data_termino" name="data_termino">
-            </div>
-
-            <div class="mb-3">
+            <div class="form-group">
                 <label for="grande_area">Grande Área: </label>
-                <input type="text" id="grande_area" name="grande_area">
+                <input class="form-control" type="text" id="grande_area" name="grande_area">
             </div>
 
-            <div class="mb-3">
+            <div class="form-group">
                 <label for="area">Área: </label>
-                <input type="text" id="area" name="area">
+                <input class="form-control" type="text" id="area" name="area">
             </div>
 
-            <div class="mb-3">
+            <div class="form-group">
                 <label for="subarea">Subarea: </label>
-                <input type="text" id="subarea" name="subarea">
+                <input class="form-control" type="text" id="subarea" name="subarea">
             </div>
 
-            <div class="mb-3">
-                <label for="ano_referencia">Ano de referência: </label>
-                <input type="number" id="ano_referencia" name="ano_referencia" minlength = 4 maxlength = 4>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="pais">País de origem: </label>
+                            <input class="form-control" type="text" id="pais" name="pais">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="uf">UF: </label>
+                            <input class="form-control" type="text" id="uf" name="uf" maxlength = 4>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div class="mb-3">
-                <label for="pais">País de origem: </label>
-                <input type="text" id="pais" name="pais">
-            </div>
-
-            <div class="mb-3">
-                <label for="uf">UF: </label>
-                <input type="text" id="uf" name="uf" maxlength = 4>
-            </div>
-
-            <div class="mb-3">
-                <label for="valor_pago">Valor da bolsa: </label>
-                <input type="text" id="valor_pago" name="valor_pago">
-            </div>
+            
         </div>
+        <div><br></div> 
         <button class="btn btn-primary" type="submit" >Cadastrar</button>
+        <div><br></div> 
     </form>
 </div>
 </x-layout>
